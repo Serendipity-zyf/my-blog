85f2dbca1ab2081325dfe4a64b20f1e166615cbe
Zyf <metazyf@gmail.com>
2024-11-28 00:11:00 +0800

    revise the post img path

--- lib/rehype-img-path.ts
+++ lib/rehype-img-path.ts
@@ -6,10 +6,8 @@ export function rehypeImgPath() {
     visit(tree, 'element', (node) => {
       if (node.tagName === 'img') {
         const src = node.properties?.src as string
-        console.log('Original image src:', src) // 添加日志
-        if (src && !src.startsWith('http') && !src.startsWith('/')) {
-          node.properties.src = `/posts/images/${src}`
-          console.log('Modified image src:', node.properties.src) // 添加日志
+        if (src && !src.startsWith('http')) {
+          node.properties.src = src.replace('../public/images/', '/images/')
         }
       }
     })
